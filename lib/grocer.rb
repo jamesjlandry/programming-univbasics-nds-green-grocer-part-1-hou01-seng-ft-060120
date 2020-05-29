bd5ae6536749b8be66229e13be9cd37a9562193b
James Landry <james.j.landry@outlook.com>
2020-05-29 20:14:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,7 +14,7 @@ def consolidate_cart(cart)
   receipt = []
   purchases = {}
   cart.each do |item|
-    purchases = find_item_by_name_in_collection(item, cart)
+    purchases = find_item_by_name_in_collection(item[:item], )
     binding.pry
   end
 end
